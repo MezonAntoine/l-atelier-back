0e5ded06f0743ea23bd888b6629567da171b3c49
AntoineMezon <antoine.mezon@epitech.eu>
2024-02-12 09:03:08 +0100

    fix(route): module.exports to export

--- src/services/api/routes/cats.ts
+++ src/services/api/routes/cats.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 import { postCatDuelValidator } from '../validators/cats.validator';
 import { getCatsController, getNextDuelController, postDuelController } from '../controllers/cats.controller';
 
-module.exports = (router: Router) => {
+export = (router: Router) => {
   router.route('/').get(getCatsController);
   router.route('/duel').get(getNextDuelController).post(postCatDuelValidator, postDuelController);
 };
